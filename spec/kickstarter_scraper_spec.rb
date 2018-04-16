d6f0756361cc504e795cc064a4dd7412b6f7576e
Taylor Donlan <taylordonlan@gmail.com>
2018-04-16 19:38:31 +0000

    automatically backed up by learn

--- spec/kickstarter_scraper_spec.rb
+++ spec/kickstarter_scraper_spec.rb
@@ -1,3 +1,21 @@
+require Nokogiri
+
+
+def create_project_hash
+  projects = {}
+ 
+  kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {}
+  end
+ 
+  # return the projects hash
+  projects
+end
+
+
+
+
 describe "#create_project_hash" do
 
   it "can be called on with no errors" do
