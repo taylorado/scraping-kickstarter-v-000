2496e074e1a7cc28c60f24934e7225de29e8d74a
Taylor Donlan <taylordonlan@gmail.com>
2018-04-16 19:40:23 +0000

    automatically backed up by learn

--- spec/kickstarter_scraper_spec.rb
+++ spec/kickstarter_scraper_spec.rb
@@ -2,6 +2,9 @@ require 'nokogiri'
 
 
 def create_project_hash
+  html = File.read('fixtures/kickstarter.html')
+
+  kickstarter = Nokogiri::HTML(html)
   projects = {}
 
   kickstarter.css("li.project.grid_4").each do |project|
